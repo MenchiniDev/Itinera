82fba580232f440fb6dc483a7009eb5519b7fdb3
MenchiniDev <107705177+MenchiniDev@users.noreply.github.com>
2024-12-08 17:53:34 +0100

    added java itinera project with spring, mongodb and neo4j. neo currently not working :)

---
+++ itinera/itinera/src/test/java/com/unipi/lsmsd/itinera/ItineraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unipi.lsmsd.itinera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItineraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
