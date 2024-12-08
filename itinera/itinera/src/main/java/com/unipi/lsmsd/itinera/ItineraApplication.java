d7baf4007d9387db694c5a2ac9b56e11e4b72b22
MenchiniDev <107705177+MenchiniDev@users.noreply.github.com>
2024-12-08 18:03:32 +0100

    working neo4j

--- itinera/itinera/src/main/java/com/unipi/lsmsd/itinera/ItineraApplication.java
+++ itinera/itinera/src/main/java/com/unipi/lsmsd/itinera/ItineraApplication.java
@@ -2,11 +2,11 @@ package com.unipi.lsmsd.itinera;
 
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import org.neo4j.driver.AuthTokens;
+import org.neo4j.driver.Driver;
+import org.neo4j.driver.GraphDatabase;
+import org.neo4j.driver.Session;
 import com.mongodb.client.MongoDatabase;
-//import org.neo4j.driver.AuthTokens;
-//import org.neo4j.driver.Driver;
-//import org.neo4j.driver.GraphDatabase;
-//import org.neo4j.driver.Session;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -27,17 +27,17 @@ public class ItineraApplication {
 		}
 
 		// Connessione a Neo4j
-//		String neo4jUri = "bolt://localhost:7687"; // Sostituisci con l'URI corretto
-//		String username = "neo4j"; // Sostituisci con il tuo username
-//		String password = "password"; // Sostituisci con la tua password
-//		try (Driver driver = GraphDatabase.driver(neo4jUri, AuthTokens.basic(username, password));
-//			 Session session = driver.session()) {
-//			String query = "RETURN 'Connessione a Neo4j riuscita'";
-//			String result = session.run(query).single().get(0).asString();
-//			System.out.println(result);
-//		} catch (Exception e) {
-//			System.err.println("Errore nella connessione a Neo4j: " + e.getMessage());
-//			e.printStackTrace();
-//		}
+		String neo4jUri = "bolt://localhost:7687"; // Sostituisci con l'URI corretto
+		String username = "neo4j"; // Sostituisci con il tuo username
+		String password = "password"; // Sostituisci con la tua password
+		try (Driver driver = GraphDatabase.driver(neo4jUri, AuthTokens.basic(username, password));
+			 Session session = driver.session()) {
+			String query = "RETURN 'Connessione a Neo4j riuscita'";
+			String result = session.run(query).single().get(0).asString();
+			System.out.println(result);
+		} catch (Exception e) {
+			System.err.println("Errore nella connessione a Neo4j: " + e.getMessage());
+			e.printStackTrace();
+		}
 	}
 }
