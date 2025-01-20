fed7131308794803e573140b6909892664a7f3a9
RossSacco <rossanahh01@gmail.com>
2025-01-20 15:55:32 +0100

    push di qualche query riguardante la relazione di follow

---
+++ ItineraJava/src/main/java/com/unipi/ItineraJava/DTO/UserDTO.java
@@ -0,0 +1,23 @@
+package com.unipi.ItineraJava.DTO;
+
+
+public class UserDTO {
+
+    private String username;
+
+    // Costruttore
+    public UserDTO(String username) {
+        this.username = username;
+    }
+
+    // Getter e Setter
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
+
+
