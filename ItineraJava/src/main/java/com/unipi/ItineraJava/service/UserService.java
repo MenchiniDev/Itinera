0dc336526742481b59e5ca13652a57c396943628
NicoBache <nicolobacherotti@gmail.com>
2025-01-21 09:08:46 +0100

    cambio nomi di 2 campi in classe user + funzione per trovare last_post di utente + risoluzione findById, cambiato attributo id in user collection

--- ItineraJava/src/main/java/com/unipi/ItineraJava/service/UserService.java
+++ ItineraJava/src/main/java/com/unipi/ItineraJava/service/UserService.java
@@ -55,9 +55,20 @@ public class UserService{
     }
 
     // Aggiorna il campo "reported" per uno specifico username
+    // DA MODIFICARE CON AUTENTICAZIONE
     public void updateReportedByUsername(String username, boolean reported) {
         userRepository.updateReportedByUsername(username, reported);
     }
+
+    //trova
+    public Last_post getLastPostByUsername(String username) {
+        //prendo user
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new IllegalArgumentException("User not found with username: " + username));
+
+         return user.getLastPost();
+    }
+
 
     public User updateLastPost(String username, String postBody) {
 
@@ -65,14 +76,14 @@ public class UserService{
         User user = userRepository.findByUsername(username)
                 .orElseThrow(() -> new IllegalArgumentException("User not found with username: " + username));
 
-        Last_post lastPost = new Last_post();
-        lastPost.setPost_body(postBody);
+        Last_post last_post = new Last_post();
+        last_post.setPost_body(postBody);
 
         String timestamp = LocalDateTime.now()
                 .format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
-        lastPost.setTimestamp(timestamp);
+        last_post.setTimestamp(timestamp);
 
-        user.setLastPost(lastPost);
+        user.setLastPost(last_post);
 
         return userRepository.save(user);
     }
