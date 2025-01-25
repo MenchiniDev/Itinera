3fb8eb7ac277ee9d32e91c110b8899fc42b58715
Lorenzo Menchini <107705177+MenchiniDev@users.noreply.github.com>
2025-01-25 10:00:18 +0100

    added addPlace

--- ItineraJava/src/main/java/com/unipi/ItineraJava/controller/PlaceController.java
+++ ItineraJava/src/main/java/com/unipi/ItineraJava/controller/PlaceController.java
@@ -1,6 +1,9 @@
 package com.unipi.ItineraJava.controller;
 
+import com.unipi.ItineraJava.DTO.PlaceDTO;
 import com.unipi.ItineraJava.model.Review;
+import com.unipi.ItineraJava.model.User;
+import com.unipi.ItineraJava.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +19,8 @@ public class PlaceController {
 
     @Autowired
     private PlaceService placeService;
+    @Autowired
+    private PostService postService;
 
     // http://localhost:8080/place/Amsterdam
     // returns a list with all places ordered by decrescent rating
@@ -27,7 +32,7 @@ public class PlaceController {
 
     // http://localhost:8080/place/search?city=Amsterdam&&category=Hotel
     // returns all category places in the city ordered by rating
-
+    // funzionante sabato
     @GetMapping("/search/{city}/{category}")
     public List<MongoPlace> getPlacesByCityAndCategory(
             @PathVariable String city,
@@ -44,6 +49,7 @@ public class PlaceController {
 
     // http://localhost:8080/place?city=Rome&category=Restaurant&minRating=4.5
     // returns correctly ordered the category of a city by order
+    //funzionante sabato
     @GetMapping
     public ResponseEntity<List<MongoPlace>> getPlacesByRating(
             @RequestParam String city,
@@ -56,6 +62,27 @@ public class PlaceController {
         }
 
         return ResponseEntity.ok(mongoPlaces);
+    }
+    //http://localhost:8080/place
+    // {
+    //    "name":"Hotel Maggico",
+    //    "address":"via degli eleocorni 55",
+    //    "city":"Pontecorvo",
+    //    "category":"Hotel"
+    //}
+    // sabato funzionante
+    @PostMapping
+    public ResponseEntity<String> createPlace(
+            @RequestHeader("Authorization") String token,
+            @RequestBody PlaceDTO place) {
+        if(User.isAdmin(token))
+        {
+            placeService.addPlace(place);
+            return ResponseEntity.ok("Place created");
+        }else
+        {
+            return ResponseEntity.badRequest().body("Unauthorized, log as an admin");
+        }
     }
 }
 
